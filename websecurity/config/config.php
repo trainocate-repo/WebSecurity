9177fc193e92fda538a2c4316def2e9fe094c170
trainocate-repo <elproj@trainocate.co.jp>
2019-12-21 13:08:23 +0900

    DBパス追加

--- websecurity/config/config.php
+++ websecurity/config/config.php
@@ -2,7 +2,7 @@
 
 define('DSN', 'mysql:dbname=websec;host=localhost;charset=utf8');
 define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'websecurity');
 define('SITE_URL', 'http://localhost:8080/websecurity/');
 
 error_reporting(E_ALL & ~E_NOTICE);
